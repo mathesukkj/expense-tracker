0d3c07dbba592299243ba004051c300dd3c2445d
mathesukkj <kemuel.g7363@gmail.com>
2023-02-15 00:12:56 -0300

    Added dynamic output via props

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,55 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const expenses = [
+        {
+            id: "e1",
+            title: "Seguro de Vida",
+            price: 300,
+            date: new Date(2023, 1, 10),
+        },
+        {
+            id: "e2",
+            title: "Mouse Pantera Pro",
+            price: 429,
+            date: new Date(2023, 2, 14),
+        },
+        {
+            id: "e3",
+            title: "Curso de React",
+            price: 22.9,
+            date: new Date(2022, 11, 17),
+        },
+        {
+            id: "e4",
+            title: "Monitor 144hz",
+            price: 1100,
+            date: new Date(2022, 10, 3),
+        },
+    ];
 
     return (
         <div className="App">
-            <ExpenseItem />
+            <ExpenseItem
+                title={expenses[0].title}
+                price={expenses[0].price}
+                date={expenses[0].date}
+            />
+            <ExpenseItem
+                title={expenses[1].title}
+                price={expenses[1].price}
+                date={expenses[1].date}
+            />
+            <ExpenseItem
+                title={expenses[2].title}
+                price={expenses[2].price}
+                date={expenses[2].date}
+            />
+            <ExpenseItem
+                title={expenses[3].title}
+                price={expenses[3].price}
+                date={expenses[3].date}
+            />
         </div>
     );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>14 de Fevereiro de 2022</div>
-            <div className="expense-item__description">
-                <h2>Seguro de Vida</h2>
-                <div className="expense-item__price">R$300.00</div>
+            <div>{props.date.toISOString().substring(0, 10)}</div>
+
+            <div className="description">
+                <h2>{props.title}</h2>
+                <div className="price">R${props.price}</div>
             </div>
         </div>
     );
