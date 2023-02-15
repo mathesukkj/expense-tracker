e2b6fa18c8d47218b8752319ace756944e45d558
mathesukkj <kemuel.g7363@gmail.com>
2023-02-14 23:50:46 -0300

    Added basic CSS

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
-    return <h2>Expense Item</h2>;
+    return (
+        <div className="expense-item">
+            <div>14 de Fevereiro de 2022</div>
+            <div className="expense-item__description">
+                <h2>Seguro de Vida</h2>
+                <div className="expense-item__price">R$300.00</div>
+            </div>
+        </div>
+    );
 }
