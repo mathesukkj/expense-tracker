406d1a9ecd8fee21ab9e3fe85065e01766dbf36b
mathesukkj <kemuel.g7363@gmail.com>
2023-02-19 13:16:40 -0300

    feat: add expenses dynamic output

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -15,26 +15,13 @@ export default function Expenses(props) {
     return (
         <Card className="expenses">
             <ExpensesFilter year={filterYear} onChangeYear={handleChangeYear} />
-            <ExpenseItem
-                title={props.expenses[0].title}
-                price={props.expenses[0].price}
-                date={props.expenses[0].date}
-            />
-            <ExpenseItem
-                title={props.expenses[1].title}
-                price={props.expenses[1].price}
-                date={props.expenses[1].date}
-            />
-            <ExpenseItem
-                title={props.expenses[2].title}
-                price={props.expenses[2].price}
-                date={props.expenses[2].date}
-            />
-            <ExpenseItem
-                title={props.expenses[3].title}
-                price={props.expenses[3].price}
-                date={props.expenses[3].date}
-            />
+            {props.expenses.map((item) => (
+                <ExpenseItem
+                    title={item.title}
+                    price={item.price}
+                    date={item.date}
+                />
+            ))}
         </Card>
     );
 }
