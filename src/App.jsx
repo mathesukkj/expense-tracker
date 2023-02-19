24028006fcdacd9e76abbeca3f3308f5721a91fd
mathesukkj <kemuel.g7363@gmail.com>
2023-02-19 13:26:36 -0300

    feat: add Expenses dynamic user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
-import ExpensesFilter from "./components/ExpensesFilter/ExpensesFilter";
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
+const expensesArray = [
+    {
+        id: "e1",
+        title: "Seguro de Vida",
+        price: 300,
+        date: new Date(2023, 1, 10),
+    },
+    {
+        id: "e2",
+        title: "Mouse Pantera Pro",
+        price: 429,
+        date: new Date(2023, 2, 14),
+    },
+    {
+        id: "e3",
+        title: "Curso de React",
+        price: 22.9,
+        date: new Date(2022, 11, 17),
+    },
+    {
+        id: "e4",
+        title: "Monitor 144hz",
+        price: 1100,
+        date: new Date(2022, 10, 3),
+    },
+];
+
 function App() {
-    const expenses = [
-        {
-            id: "e1",
-            title: "Seguro de Vida",
-            price: 300,
-            date: new Date(2023, 1, 10),
-        },
-        {
-            id: "e2",
-            title: "Mouse Pantera Pro",
-            price: 429,
-            date: new Date(2023, 2, 14),
-        },
-        {
-            id: "e3",
-            title: "Curso de React",
-            price: 22.9,
-            date: new Date(2022, 11, 17),
-        },
-        {
-            id: "e4",
-            title: "Monitor 144hz",
-            price: 1100,
-            date: new Date(2022, 10, 3),
-        },
-    ];
+    const [expenses, setExpenses] = useState(expensesArray);
 
     function handleAddExpense(expenseData) {
-        console.log(expenseData);
+        setExpenses((prevState) => {
+            return [expenseData, ...prevState];
+        });
     }
 
     return (
